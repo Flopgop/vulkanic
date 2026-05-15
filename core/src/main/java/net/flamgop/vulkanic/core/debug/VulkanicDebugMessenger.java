58cd8cacfad70947ba3bf45aaaefca00863a6006
Flopgop <137199787+Flapgop@users.noreply.github.com>
2026-05-15 13:00:58 -0700

    docs: debug messenger

--- core/src/main/java/net/flamgop/vulkanic/core/debug/VulkanicDebugMessenger.java
+++ core/src/main/java/net/flamgop/vulkanic/core/debug/VulkanicDebugMessenger.java
@@ -3,5 +3,11 @@ package net.flamgop.vulkanic.core.debug;
 import net.flamgop.vulkanic.util.EnumIntBitset;
 
 public interface VulkanicDebugMessenger {
+    /**
+     * @param severity message severity
+     * @param type message type
+     * @param callbackData related message data
+     * @return false. True is technically UB
+     */
     boolean message(EnumIntBitset<VulkanicDebugMessageSeverityFlag> severity, EnumIntBitset<VulkanicDebugMessageTypeFlag> type, VulkanicDebugCallbackData callbackData);
 }
