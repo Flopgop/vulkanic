5534185cd06250924d92a1adbb5c076f74c24c54
Flopgop <76978462+Flopgop@users.noreply.github.com>
2026-09-16 00:04:20 -0700

    fix: clarify VulkanicAllocationCreateInfo and add alternate constructor

--- core/src/main/java/net/flamgop/vulkanic/memory/VulkanicAllocationCreateInfo.java
+++ core/src/main/java/net/flamgop/vulkanic/memory/VulkanicAllocationCreateInfo.java
@@ -3,11 +3,19 @@ package net.flamgop.vulkanic.memory;
 import net.flamgop.vulkanic.util.EnumIntBitset;
 
 /// Note: Memory usage is effectively automatic in VMA based on requiredFlags and preferredFlags
+/// @apiNote A general standard for `createBuffer` and `createImage` methods is that a `memoryTypeBits` of 0 should be interpreted as "autodetect"
 public record VulkanicAllocationCreateInfo(
         EnumIntBitset<VulkanicMemoryPropertyFlag> requiredFlags,
         EnumIntBitset<VulkanicMemoryPropertyFlag> preferredFlags,
         int memoryTypeBits,
         float priority
-        // TODO: determine if pool, priority, and pUserData matter
+        // TODO: determine if pool and pUserData matter
 ) {
+    public VulkanicAllocationCreateInfo(
+            EnumIntBitset<VulkanicMemoryPropertyFlag> requiredFlags,
+            EnumIntBitset<VulkanicMemoryPropertyFlag> preferredFlags,
+            float priority
+    ) {
+        this(requiredFlags, preferredFlags, 0, priority);
+    }
 }
