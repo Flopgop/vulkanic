7a1be871678548416cffa7aef8d6296cc7eac972
Flopgop <76978462+Flopgop@users.noreply.github.com>
2026-09-20 14:57:56 -0700

    feat: index type size func

--- core/src/main/java/net/flamgop/vulkanic/memory/VulkanicIndexType.java
+++ core/src/main/java/net/flamgop/vulkanic/memory/VulkanicIndexType.java
@@ -5,18 +5,23 @@ import org.lwjgl.vulkan.VK10;
 import org.lwjgl.vulkan.VK14;
 
 public enum VulkanicIndexType {
-    UINT16(VK10.VK_INDEX_TYPE_UINT16),
-    UINT32(VK10.VK_INDEX_TYPE_UINT32),
+    UINT16(VK10.VK_INDEX_TYPE_UINT16, 2),
+    UINT32(VK10.VK_INDEX_TYPE_UINT32, 4),
 
-    UINT8(VK14.VK_INDEX_TYPE_UINT8),
+    UINT8(VK14.VK_INDEX_TYPE_UINT8, 1),
 
-    NONE(KHRAccelerationStructure.VK_INDEX_TYPE_NONE_KHR)
+    NONE(KHRAccelerationStructure.VK_INDEX_TYPE_NONE_KHR, 0)
     ;
     private final int qualifier;
-    VulkanicIndexType(int qualifier) {
+    private final long size;
+    VulkanicIndexType(int qualifier, long size) {
         this.qualifier = qualifier;
+        this.size = size;
     }
 
+    public long size() {
+        return size;
+    }
     public int qualifier() {
         return qualifier;
     }
